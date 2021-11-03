71a5a3ff90e311ffad97195c90d4e91b081efff8
paulo-almeida <paulo.almeida@zukk.com.br>
2021-11-03 19:13:46 -0300

    PokemonCard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar'
 import Background from '../../components/Background'
 import { HomeContainer } from './styles'
+import { useTheme } from 'styled-components'
+import PokemonCard from '../../components/PokemonCard'
 
 const Home = () => {
+  const theme = useTheme()
+  const [badges] = useState([
+    {
+      id: 1,
+      name: 'Grass',
+      color: theme.color.type.grass,
+    },
+    {
+      id: 2,
+      name: 'Poison',
+      color: theme.color.type.poison,
+    },
+  ])
   return (
     <>
       <Background />
       <HomeContainer>
         <Navbar />
-        <div>Home</div>
+        <PokemonCard code="#1" name="Bulbasaur" badges={badges} />
       </HomeContainer>
     </>
   )
